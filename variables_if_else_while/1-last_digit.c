a0c700c98fcca8a63eb97188f16d0983f6c35bcb
TWA-Coder <twakaru744@gmail.com>
2025-09-13 18:26:47 +0200

    alphabet

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,21 +10,21 @@
 int main(void)
 {
 long int n;
-int last_digit;
+int lt_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last_digit = n % 10;
-if (last_digit > 5)
+lt_digit = n % 10;
+if (lt_digit > 5)
 {
-printf("Last digit of %ld is %d and is greater than 5\n", n, last_digit);
+printf("Last digit of %ld is %d and is greater than 5\n", n, lt_digit);
 }
-else if (last_digit == 0)
+else if (lt_digit == 0)
 {
-printf("Last digit of %ld is %d and is 0\n", n, last_digit);
+printf("Last digit of %ld is %d and is 0\n", n, lt_digit);
 }
 else
 {
-printf("Last digit of %ld is %d and is less than 6 and not 0", n, last_digit);
+printf("Last digit of %ld is %d and is less than 6 and not 0\n", n, lt_digit);
 }
 return (0);
 }
