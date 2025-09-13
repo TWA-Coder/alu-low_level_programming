b5092783d9f051cc7a11f40b360f5090ab98c2d7
TWA-Coder <twakaru744@gmail.com>
2025-09-13 11:16:57 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,8 +4,9 @@
 *
 * return: it return zero
 */
+
 int main(void)
 {
-    puts ("Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
