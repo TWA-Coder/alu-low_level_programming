c7f9aa5d8cb5e04dd2db3c6cda6167c28292004e
TWA-Coder <twakaru744@gmail.com>
2025-09-13 11:50:10 +0200

    printf

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - is a function that prints the size of different data-types
+*
+*Return: 0 if no error occurs and a non-zero value otherwise
+*/
+
+int main(void)
+{
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of a int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
+}
