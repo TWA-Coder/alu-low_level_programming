52230a1c4e3b1ac60b6ff9e3f11cb87d533d89eb
TWA-Coder <twakaru744@gmail.com>
2025-09-13 11:26:41 +0200

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - will print the given input to the console
+*
+*Return: 0 if the program runs successfully
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
